4526b95f1f567e9bf50a019e2e3d7b53fc279f49
luisrodbert <93887654+LUISRGUEZSOSA@users.noreply.github.com>
2024-06-19 08:06:26 +0100

    Actualizar Controllers

---
+++ React_Backend/Api/Controllers/Planes.controller.js
@@ -0,0 +1,68 @@
+const Flights = require("../Models/Flights.model");
+
+
+const getAllFlights = async (req, res) => {
+  try {
+    const Flights = await Flights.findAll();
+    return res.status(200).json(flights);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getOneFlights = async (req, res) => {
+  try {
+    const oneFlights = await Flights.findByPk(req.params.id);
+    if (oneFlights) {
+      return res.status(200).json(oneFlights);
+    } else {
+      return res.status(200).send("No user found");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateFlights = async (req, res) => {
+  try {
+    const updatedFlights = await Flights.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    return res.status(200).json(updatedFlights);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+const deleteFlights = async (req, res) => {
+  try {
+    const deletedFlights = await Flights.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    return res.status(200).json(deletedFlights);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error.message);
+  }
+};
+
+const createFlights = async (req, res) => {
+  try {
+    const updatedFlights = await Flights.create(req.body);
+    return res.status(200).json(updatedFlights);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+module.exports = {
+  getAllFlights,
+  getOneFlights,
+  updateFlights,
+  deleteFlights,
+  createFlights,
+};
