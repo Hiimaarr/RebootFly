46d4d58f01f5630e2e6c71ffd500f3b557804d26
luisrodbert <93887654+LUISRGUEZSOSA@users.noreply.github.com>
2024-06-19 08:08:35 +0100

    Actualizar index.js

--- React_Backend/Api/Routes/index.js
+++ React_Backend/Api/Routes/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.use('/Airport', require('./Airport.router'));
 router.use("/Flights", require("./Flights.router"));
+router.use("/Location", require("./Location.router"));
 
 module.exports = router;
