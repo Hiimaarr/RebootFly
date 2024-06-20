3907c30d8d0a7322a40b35916f0584df165339aa
reboot <luisguezsosa@gmail.com>
2024-06-20 16:09:06 +0100

    GENERAL: -Doing relations

--- React_Backend/Database/relations.js
+++ React_Backend/Database/relations.js
@@ -1,22 +1,15 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const { sequelize } = require("./index");
 const Airport = require("../Api/Models/Airport.model");
-const Billing = require("../Api/Models/Billing.model");
-const Clients = require("../Api/Models/Clients.model");
-const Employees = require("../Api/Models/Employees.model");
+const Bookings = require("../Api/Models/Bookings.model");
 const Flights = require("../Api/Models/Flights.model");
 const Location = require("../Api/Models/Location.model");
-const Planes = require("../Api/Models/Planes.model");
-const Clients_flights = require("../Api/Models/Clients_flights.model");
-const Employees_flights = require("../Api/Models/Employees_flights.model");
+const Users = require("../Api/Models/Users.model");
 
 const initializeRelations = () => {
   try {
-    Planes.hasMany(Flights);
-    Flights.belongsTo(Planes);
-
-    Location.hasMany(Flights);
-    Flights.belongsTo(Location);
+    Airport.hasMany(Flights);
+    Flights.belongsTo(Airport);
 
     Airport.hasOne(Location);
     Location.belongsTo(Airport);
