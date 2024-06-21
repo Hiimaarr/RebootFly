1ed2fe6e6d32c97dbc7443e7997cafd7a2963e99
Hiimaarr <himarllarenaalamo@gmail.com>
2024-06-21 17:01:37 +0100

    Page PriceComparator fixed

--- React_Frontend/src/Pages/PriceComparator/PriceComparator.jsx
+++ React_Frontend/src/Pages/PriceComparator/PriceComparator.jsx
@@ -1,17 +1,27 @@
-import FlightCard from "../../components/FlightCard/FlightCard";
-import "./PriceComparator.css"
+import FlightCard from '../../components/FlightCard/FlightCard';
+import HeaderResultados from '../../components/HeaderResultados/HeaderResultados';
+import './PriceComparator.css';
 
 function PriceComparator() {
   return (
     <div id="comparatorContent">
       <div id="Resultados">
-        <div id="InfoResultados"></div>
+
+        <div id="HrContainer">
+          <HeaderResultados />
+        </div>
+
         <div id="FlightsContainers">
           <FlightCard />
         </div>
+
+      </div>
+
+      <div id='ResumenContainer'>
+
       </div>
     </div>
   );
 }
 
-export default PriceComparator
+export default PriceComparator;
