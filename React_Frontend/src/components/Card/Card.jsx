5884e66591f139fa0ddede738829ab15c0e4fa1b
Hiimaarr <himarllarenaalamo@gmail.com>
2024-06-22 18:52:50 +0000

    Card 1

--- React_Frontend/src/components/Card/Card.jsx
+++ React_Frontend/src/components/Card/Card.jsx
@@ -1,5 +1,3 @@
-import Search from '../../components/Search/Search';
-import './Home.css';
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -8,16 +6,9 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-
-function Home() {
+export default function MediaCard() {
   return (
-    <>
-      <div>
-        <div id="ContenidoPagina">
-          <Search/>
-          <section id="Content">
-            <div id=" Oferta1">
-            <Card sx={{ maxWidth: 345, marginTop:"20px"}}>
+    <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
         image="/static/images/cards/contemplative-reptile.jpg"
@@ -25,7 +16,7 @@ function Home() {
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Madrid
+          Lizard
         </Typography>
         <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
@@ -33,24 +24,13 @@ function Home() {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Reservar</Button>
-        <span size="small">Learn More</span>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
-            </div>
-            <div id=" Oferta2 ">Oferta 2</div>
-            <div id=" Oferta3 ">Oferta 3</div>
-            <div id=" Oferta4 ">Oferta 4</div>
-            <div id=" Oferta5 ">Oferta 5</div>
-            <div id=" Oferta6 ">Oferta 6</div>
-          </section>
-        <section id="Promociones">
-          <div id="inputCorreo">Aqui va para meter el correo</div>
-        </section>
-        </div>
-      </div>
-    </>
   );
 }
 
-export default Home;
+module.exports = {
+    MediaCard,
+}
