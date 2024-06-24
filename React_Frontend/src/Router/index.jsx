b75b4c7ddcbdd7298c23007d092dceed1f388874
David García <169043049+DaviidGarciia@users.noreply.github.com>
2024-06-24 18:38:11 +0000

    index router fixed

--- React_Frontend/src/Router/index.jsx
+++ React_Frontend/src/Router/index.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 /* import App from "../App"; */
 import Layout from "../Layout/Layout";
 import Notfound from "../Pages/NotFound/Notfound";
-<<<<<<< HEAD
 import Data from "../Pages/Data/Data";
 import SignUp from "../Pages/Signup/Signup";
 import Login from "../Pages/Login/Login"
@@ -10,38 +9,26 @@ import Payment from "../Pages/Payment/Payment";
 import Confirmation from "../Pages/Confirmation/Confirmation";
 import Profile from "../Pages/Profile/Profile";
 import Admin from "../Pages/Admin/Admin"
-=======
 import Home from "../Pages/Home/Home";
 import PriceComparator from "../Pages/PriceComparator/PriceComparator";
->>>>>>> e8dd63269f5afe5c7ec8d35d85990cb1463e850f
 const router = createBrowserRouter([
 {
     path:"/",
     element:<Layout/>,
     errorElement:<Notfound/>,
-<<<<<<< HEAD
-    children:[/*
-=======
     children:[
->>>>>>> e8dd63269f5afe5c7ec8d35d85990cb1463e850f
     {
         path:"/",
         element:<Home/>
     }, 
     {
-<<<<<<< HEAD
-        path:"/price"
-        element: página de comparador de precios
-    },*/
-    {
         path:"/data",
         element: <Data/>
     },
-=======
+    {
         path:"/price",
         element:<PriceComparator/>
-    },]/*
->>>>>>> e8dd63269f5afe5c7ec8d35d85990cb1463e850f
+    },
     {
         path:"/confirmation",
         element: <Confirmation/>
@@ -50,8 +37,7 @@ const router = createBrowserRouter([
         path:"/payment",
         element:<Payment/>
     }
-    ]
-},
+]},
 {
     path:"/signup",
     element:<SignUp/>
@@ -67,8 +53,8 @@ const router = createBrowserRouter([
 {
     path:"/Admin",
     element:<Admin/>
-}
-])
+}]
+)
 
 
 export default router
