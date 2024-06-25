c10e22c2252ea2e88d726b552588c8c7bb707c7a
luisrodbert <93887654+LUISRGUEZSOSA@users.noreply.github.com>
2024-06-25 11:33:50 +0000

    General: -we have create the searchflight controller

--- React_Backend/Api/Routes/index.js
+++ React_Backend/Api/Routes/index.js
@@ -6,6 +6,7 @@ router.use("/Bookings", require("./Bookings.router"));
 router.use("/Location", require("./Location.router"));
 router.use("/Users", require("./Users.router"));
 router.use("/auth", require("./auth.router"));
+
 /*router.use("/FlightBooking", require("./FlightBookings.router"))*/
 
 module.exports = router;
